c9f45554ac71b9d89bb699b3c08e3ef7684d15c3
Alexander De Abreu <aledabreupt@gmail.com>
2022-11-16 12:16:17 +0100

    Add vitest, tsconfig config for tests and hello-world test, fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,30 @@
+/// <reference types="vitest" />
+import { join } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: [
+      {
+        find: /~(.+)/,
+        replacement: join(__dirname, '/node_modules/$1')
+      },
+
+      {
+        find: /@\//,
+        replacement: join(__dirname, './')
+      }
+    ]
+  },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    coverage: {
+      reporter: ['text', 'json', 'html']
+    }
+  },
   plugins: [vue()],
   css: {
     preprocessorOptions: {
