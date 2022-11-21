024dfa02247cfcb660a3b4a3f03d4cbdcd30b8f5
Alexander De Abreu <aledabreupt@gmail.com>
2022-11-21 17:08:17 +0100

    Add Navbar component and styles, Update APP with calculator-page and navbar-component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <div />
+  <NavbarComponent />
+  <calculator-page />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import CalculatorPage from '@/page/Calculator/CalculatorPage.vue'
+import NavbarComponent from '@/components/template/Navbar/NavbarComponent.vue'
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/components/template/Navbar/NavbarComponent.vue
@@ -0,0 +1,43 @@
+<template>
+  <header class="navbar">
+    <img
+      src="@/assets/logo-innocv.png"
+      class="navbar__logo"
+      alt="innocv-logo"
+      loading="lazy"
+    />
+    <label class="navbar__toggle">
+      <div
+        class="navbar__toggle--container"
+        :class="[isDark ? 'enabled' : 'disabled']"
+      >
+        <div className="navbar__toggle--container--icons">
+          <SunIcon />
+          <MoonIcon />
+        </div>
+        <input
+          type="checkbox"
+          :checked="isDark"
+          @click="toggleDark()"
+        />
+      </div>
+    </label>
+  </header>
+</template>
+
+<script setup lang="ts">
+import MoonIcon from '@/assets/moon.svg?component'
+import SunIcon from '@/assets/sun.svg?component'
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark({
+  selector: 'body', //element to add attribute to
+  attribute: 'theme', // attribute name
+  valueDark: 'dark', // attribute value for dark mode
+  valueLight: 'light' // attribute value for light mode
+})
+
+const toggleDark = useToggle(isDark)
+</script>
+
+<style lang="scss" scoped src="./NavbarComponent.scss"></style>
